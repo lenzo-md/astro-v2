c6edf22868c631dcffa038cb51091d1c8e095c8a
astro-md <106902912+rikkubot@users.noreply.github.com>
2024-05-10 23:03:46 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -34,7 +34,7 @@ command({
     await message.sendMessage("```Pɪɴɢ!```");
     const end = new Date().getTime();
     const ms = end - start;
-    return await message.client.sendMessage(message.jid, { location: { degreesLatitude: 24.121231, degreesLongitude: 55.1121221, name: `Pᴏɴɢ ${ms}ᴍꜱ`, address: "Λsᴛʀᴏ xᴅ"} });
+    return await message.client.sendMessage(message.jid, { location: { degreesLatitude: 24.121231, degreesLongitude: 55.1121221, name: `Pᴏɴɢ ${ms}ᴍꜱ`, address: "Λsᴛʀᴏ Xᴅ"} });
 });
 
 /* Copyright (C) 2022 X-Electra.
